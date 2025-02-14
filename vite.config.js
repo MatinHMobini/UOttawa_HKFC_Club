50f50fb66443d1e0492816b024f3267a22c329e2
Matin Mobini <mhass158@uottawa.ca>
2025-02-13 23:06:46 -0500

    path update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Use relative path for GitHub Pages deployment
+  base: '/UOttawa_HKFC_Club/',
 });
