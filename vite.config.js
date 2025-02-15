4b75d05b9a003e9302bc4bfe6e6e67029f77d2cc
Matin Mobini <mhass158@uottawa.ca>
2025-02-14 19:21:33 -0500

    retry

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Use relative path for GitHub Pages deployment
+  base: "/UOttawa_HKFC_Club/",  // <-- Ensure this matches your GitHub repository name
 });
